bf65de4ed758ec45a217c7318843d53c46faa3d1
Mitch Thomas <mthomas@tripwire.com>
2020-01-15 23:06:29 -0800

    Use docker compose to setup a keycloak server to test

--- build-run.sh
+++ build-run.sh
@@ -1,6 +1,8 @@
 # Build the image locally
-docker build . -t keycloak-react-app:dev
+# docker build . -t keycloak-react-app:dev
 # Run the newly built image for test purposes
-docker run -d -p 3000:80 -v $PWD/keycloak.json:/usr/share/nginx/html/config/keycloak.json keycloak-react-app:dev
-# On a Mac this opens Safari 
+# docker run -d -p 3000:80 -v $PWD/keycloak.json:/usr/share/nginx/html/config/keycloak.json keycloak-react-app:dev
+
+docker-compose -f docker-compose.yaml up --build
+# On a Mac this opens Safari
 open http://localhost:3000
