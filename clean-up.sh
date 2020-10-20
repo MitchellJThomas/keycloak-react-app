3a15dd9f331537a5ae51e65cf5bf9bcde056fe02
Mitch Thomas <mthomas@tripwire.com>
2020-10-20 14:17:23 -0700

    Be specific about which posgres image to delete

--- clean-up.sh
+++ clean-up.sh
@@ -3,4 +3,4 @@ docker volume rm keycloak-react-app_postgres_data
 docker network rm keycloak-react-app_default
 docker rmi keycloak-react-app:latest
 docker rmi quay.io/keycloak/keycloak:8.0.1
-docker rmi postgres:latest
+docker rmi postgres:13
