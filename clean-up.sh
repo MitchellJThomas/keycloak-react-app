75a6d6d0738a5f461295a73f812c4f86bd5ee2b0
Mitch Thomas <mthomas@tripwire.com>
2021-10-12 18:09:33 -0700

    Cleanup the proper Keycloak image

--- clean-up.sh
+++ clean-up.sh
@@ -2,5 +2,5 @@ docker-compose rm -f -v -s
 docker volume rm keycloak-react-app_postgres_data
 docker network rm keycloak-react-app_default
 docker rmi keycloak-react-app:latest
-docker rmi quay.io/keycloak/keycloak:8.0.1
+docker rmi quay.io/keycloak/keycloak@sha256:b4a1ec2d93477ef32587bb29932e9567f967919a0a2d6cd54748060f90180b35
 docker rmi postgres:13
